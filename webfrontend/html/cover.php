82d13298da6bc84f6cb78e0c5ba19431573018c1
mschlenstedt <Michael@schlenn.net>
2026-04-20 21:58:52 +0200

    debug: enable error display temporarily

--- webfrontend/html/cover.php
+++ webfrontend/html/cover.php
@@ -1,6 +1,6 @@
 <?php
-ini_set('display_errors', '0');
-error_reporting(0);
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
 // cover.php — Serve resized album cover for a given zone
 
 // ── GET parameters ────────────────────────────────────────────
